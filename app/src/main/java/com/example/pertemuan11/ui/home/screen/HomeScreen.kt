aee250611fa3e3c9a284d8a0c8c95d9637ba2803
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 05:03:01 +0700

    menambahkan ondeleteclick, spacer dan iconbutton pada function KontakCard

--- app/src/main/java/com/example/pertemuan11/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pertemuan11/ui/home/screen/HomeScreen.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -95,6 +97,7 @@ fun KontakLayout(kontak: List<Kontak>, modifier: Modifier = Modifier) {
 @Composable
 fun KontakCard(
     kontak: Kontak,
+    onDeleteClick: (Kontak) -> Unit = {},
     modifier: Modifier = Modifier
 ) {
     Card(
@@ -114,6 +117,12 @@ fun KontakCard(
                     style = MaterialTheme.typography.titleLarge,
                 )
                 Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(kontak) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
                 Icon(
                     imageVector = Icons.Default.Phone,
                     contentDescription = null,
