7b117352c3ab1efd08f1546338fa20622b806f6c
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 05:32:27 +0700

    menambahkan val scrollBehavior dan menambahkan parameter scaffold pada HomeScreen

--- app/src/main/java/com/example/pertemuan11/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pertemuan11/ui/home/screen/HomeScreen.kt
@@ -14,18 +14,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -34,6 +39,7 @@ import com.example.pertemuan11.R
 import com.example.pertemuan11.model.Kontak
 import com.example.pertemuan11.navigation.DestinasiNavigasi
 import com.example.pertemuan11.ui.PenyediaViewModel
+import com.example.pertemuan11.ui.TopAppBarKontak
 import com.example.pertemuan11.ui.home.viewmodel.HomeViewModel
 import com.example.pertemuan11.ui.home.viewmodel.KontakUIState
 
@@ -48,7 +54,28 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     onDetailClick: (Int) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {}
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        },
+    ) {}
+}
 
 @Composable
 fun HomeStatus(
