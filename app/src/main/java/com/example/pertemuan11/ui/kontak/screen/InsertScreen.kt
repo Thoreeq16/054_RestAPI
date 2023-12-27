0432e5880eb4c510da5674c0d8426e507b328c7c
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 06:06:15 +0700

    memberi parameter pada fun EntryKontakBody

--- app/src/main/java/com/example/pertemuan11/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pertemuan11/ui/kontak/screen/InsertScreen.kt
@@ -1,20 +1,28 @@
 package com.example.pertemuan11.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan11.navigation.DestinasiNavigasi
 import com.example.pertemuan11.ui.PenyediaViewModel
 import com.example.pertemuan11.ui.TopAppBarKontak
+import com.example.pertemuan11.ui.kontak.viewmodel.InsertUiEvent
+import com.example.pertemuan11.ui.kontak.viewmodel.InsertUiState
 import com.example.pertemuan11.ui.kontak.viewmodel.InsertViewModel
 import kotlinx.coroutines.launch
 
@@ -57,5 +65,31 @@ fun EntryKontakScreen(
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputSiswa(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
     }
 }
