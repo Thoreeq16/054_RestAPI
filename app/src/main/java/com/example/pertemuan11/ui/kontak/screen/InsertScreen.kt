e29944c9f25d6b2bfa40e73ca9581af46fed0ad2
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 05:57:44 +0700

    membuat fun EntryKontakScreen

--- app/src/main/java/com/example/pertemuan11/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pertemuan11/ui/kontak/screen/InsertScreen.kt
@@ -1,8 +1,12 @@
 package com.example.pertemuan11.ui.kontak.screen
 
+import androidx.compose.runtime.Composable
 import com.example.pertemuan11.navigation.DestinasiNavigasi
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Siswa"
 }
+
+@Composable
+fun EntryKontakScreen() {}
