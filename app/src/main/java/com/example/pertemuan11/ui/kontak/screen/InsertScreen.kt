fd8ded6da2bf2a3c4056208ce795ba14b56347a8
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 05:59:08 +0700

    membuat parameter EntryKontakScreen

--- app/src/main/java/com/example/pertemuan11/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pertemuan11/ui/kontak/screen/InsertScreen.kt
@@ -1,7 +1,11 @@
 package com.example.pertemuan11.ui.kontak.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan11.navigation.DestinasiNavigasi
+import com.example.pertemuan11.ui.PenyediaViewModel
+import com.example.pertemuan11.ui.kontak.viewmodel.InsertViewModel
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
@@ -9,4 +13,8 @@ object DestinasiEntry : DestinasiNavigasi {
 }
 
 @Composable
-fun EntryKontakScreen() {}
+fun EntryKontakScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
