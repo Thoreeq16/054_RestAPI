0ebfa9ce8619ff02340b621d9cd73ff3913fb8f6
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 05:44:04 +0700

    membuat kelas InsertViewModel

--- app/src/main/java/com/example/pertemuan11/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.pertemuan11.ui.kontak.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan11.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
