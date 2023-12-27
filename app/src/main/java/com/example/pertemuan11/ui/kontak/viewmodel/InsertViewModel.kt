b97ac928f0d5cec958e1b941c896091295982bc2
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 05:45:24 +0700

    membuat data class InsertUiEvent

--- app/src/main/java/com/example/pertemuan11/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/kontak/viewmodel/InsertViewModel.kt
@@ -28,3 +28,10 @@ class InsertViewModel(private val kontakRepository: KontakRepository) : ViewMode
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = ""
+)
