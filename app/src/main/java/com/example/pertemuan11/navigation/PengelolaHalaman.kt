52e0557222f786ead8eed442d096d81842f6c3e2
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-28 06:16:46 +0700

    mengisi parameter pada fun PengelolaHalaman

--- app/src/main/java/com/example/pertemuan11/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan11/navigation/PengelolaHalaman.kt
@@ -1,6 +1,37 @@
 package com.example.pertemuan11.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan11.ui.home.screen.DestinasiHome
+import com.example.pertemuan11.ui.home.screen.HomeScreen
+import com.example.pertemuan11.ui.kontak.screen.DestinasiEntry
+import com.example.pertemuan11.ui.kontak.screen.EntryKontakScreen
 
 @Composable
-fun PengelolaHalaman() {}
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {}
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryKontakScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
